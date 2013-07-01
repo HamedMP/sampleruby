40ec9481b6ccdf3cfac02672fa810ac7dafe4842
Hamed <hamedmp2012@gmail.com>
2013-07-01 23:00:33 +0500

    INit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Samp3::Application.config.secret_key_base = '16d29cbab59807c5ce63934fd212342aebfc2ba9216d5d14721d4bc1b66e9203010a8411b17b67ea1b379ad598dc8cad9e92d389a0866559e552e1f596653a73'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Samp3::Application.config.session_store :cookie_store, key: '_samp3_session'
